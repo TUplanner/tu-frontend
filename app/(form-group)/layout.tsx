d60655690404b494bf30cc1908bd7f3707c77188
VJL0 <jlvictor2020@gmail.com>
2024-09-08 20:41:42 -0400

    add form/nav layout

---
+++ app/(form-group)/layout.tsx
@@ -0,0 +1,10 @@
+const NavLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <section>
+      <nav>hey</nav>
+
+      {children}
+    </section>
+  );
+};
+export default NavLayout;
