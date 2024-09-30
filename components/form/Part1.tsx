6719407ff10309ebd178c4559a3d7b19cd6964a4
VJL0 <jlvictor2020@gmail.com>
2024-09-30 01:50:17 -0400

    revert to RESTful API

--- components/form/Part1.tsx
+++ components/form/Part1.tsx
@@ -1,45 +1,9 @@
-import Dropdown from "@/components/Dropdown";
-import { apiFetch } from "@/lib/utils";
+import React from 'react'
 
-interface Data {
-  code: string;
-  description: string;
-}
-
-interface ProgramTermData {
-  programs: Data[];
-  terms: Data[];
-}
-
-const QUERY = `
-  query {
-    programs {
-      description
-    }
-    terms {
-      description
-    }
-  }
-`;
-
-const Part1 = async () => {
-  let programs: string[] = [];
-  let terms: string[] = [];
-
-  try {
-    const data: ProgramTermData = await apiFetch(QUERY);
-    programs = data.programs.map((item) => item.description);
-    terms = data.terms.map((item) => item.description);
-  } catch (error) {
-    console.error("Failed to fetch data:", error);
-  }
-
+const Part1 = () => {
   return (
-    <>
-      <Dropdown name="program" items={programs} search classname="!w-96" />
-      <Dropdown name="term" items={terms} />
-    </>
-  );
-};
+    <div>Part1</div>
+  )
+}
 
-export default Part1;
+export default Part1
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,16 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const apiFetch = async (query: string) => {
-  const response = await fetch(String(process.env.API_URL), {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query }),
-  });
-
-  const { data } = await response.json();
-  return data;
-};
